f744985a0dc91b6b678a5a42b236d961c4f5c712
Eric Hambro <eric.hambro@gmail.com>
2017-12-02 13:12:34 +0000

    git prompt

--- setup.sh
+++ setup.sh
@@ -5,5 +5,11 @@ for f in *[^md]; do
   ln -s `pwd`"/$f" "$HOME/.$f"
 done
 
+echo "Installing... Vundle"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+echo "Done. Run \":PluginInstall\" on first Vim Run"
 
+echo "Getting... Git Prompts"
+curl -o $HOME/.git-completion.bash  https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+curl -o $HOME/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+echo "Done"
