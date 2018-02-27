c4ea7e4f245917fe23223268ab8d7b45530d821e
Eric Hambro <eric.hambro@gmail.com>
2018-02-27 12:25:04 +0000

    add autogen from git ignore, using githubs gitignores

---
+++ gitignore.sh
@@ -0,0 +1,8 @@
+function gitignore (){
+   gi_file=$(shopt -s nocaseglob; ls ~/Programming/ThirdParty/gitignore/$1\.gitignore)
+   if [ -f ./.gitignore ]; then
+     echo "gitignore already exists" && return 1
+   else
+     cp  $gi_file ./.gitignore && echo "copied $1 to .gitignore" && ln -s .gitignore $1.gitignore
+   fi
+ }
